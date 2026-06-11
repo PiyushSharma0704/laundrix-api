1fd96a5a191faa3ec761ee3cc06dbf570f683d9e
Piyush Sharma <piyushsharma@Mac.lan>
2026-06-12 02:43:34 +0530

    feat: initialize laundrix api with express typescript and prisma

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+import routes from "./routes";
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(compression());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api", routes);
+
+export default app;
